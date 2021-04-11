11529e512dd22f322ea936d7449e828136ffb162
WLM1ke <wlmike@gmail.com>
2021-04-11 12:16:38 +0300

    feat: обработка не найденного ответа

--- data/app/server.go
+++ data/app/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 	"net/http"
 	"poptimizer/data/adapters"
@@ -45,45 +46,31 @@ func (s *Server) logger(handler http.Handler) http.Handler {
 }
 
 func (s *Server) Start(ctx context.Context) error {
-	r := chi.NewRouter()
+	router := chi.NewRouter()
 	// TODO: добавить другие middleware
-	r.Use(s.logger)
-	r.Use(middleware.RedirectSlashes)
-	r.Get("/{group}/{name}", func(w http.ResponseWriter, r *http.Request) {
+	router.Use(s.logger)
+	router.Use(middleware.RedirectSlashes)
+	router.Get("/{group}/{name}", func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithTimeout(context.Background(), s.requestTimeout)
 		defer cancel()
 		res, err := s.repo.ViewJOSN(ctx, domain.TableID{domain.Group(chi.URLParam(r, "group")), domain.Name(chi.URLParam(r, "name"))})
-		if err != nil {
+		if err == mongo.ErrNoDocuments {
+			router.NotFoundHandler()(w, r)
+			return
+		} else if err != nil {
+			// Как сообщать об ошибке
+			// https://golang.org/src/net/http/server.go?s=64501:64553#L2068
 			zap.L().Panic(s.Name(), zap.Error(err))
 		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		_, err = w.Write(res)
 		if err != nil {
 			zap.L().Panic(s.Name(), zap.Error(err))
 		}
 	})
 
-	// Как писать JSON
-	//func JSON(w http.ResponseWriter, r *http.Request, v interface{}) {
-	//buf := &bytes.Buffer{}
-	//enc := json.NewEncoder(buf)
-	//enc.SetEscapeHTML(true)
-	//if err := enc.Encode(v); err != nil {
-	//http.Error(w, err.Error(), http.StatusInternalServerError)
-	//return
-	//}
-	//
-	//w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	//if status, ok := r.Context().Value(StatusCtxKey).(int); ok {
-	//w.WriteHeader(status)
-	//}
-	//w.Write(buf.Bytes())
-	//}
-
-	// Как сообщать об ошибке
-	// https://golang.org/src/net/http/server.go?s=64501:64553#L2068
-
 	// Добавить настройки кастомного сервера
-	srv := http.Server{Addr: s.addr, Handler: r}
+	srv := http.Server{Addr: s.addr, Handler: router}
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
