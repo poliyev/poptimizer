a498ce6299a6e994c9eef37ca26227fa02ee4ad2
WLM1ke <wlmike@gmail.com>
2021-04-12 10:37:15 +0300

    feat: добавлен параметр количества соединений

--- data/adapters/iss.go
+++ data/adapters/iss.go
@@ -3,16 +3,14 @@ package adapters
 import (
 	"github.com/WLM1ke/gomoex"
 	"net/http"
-	"time"
 )
 
 // NewISSClient - создает клиент для ISS с ограничением на количество соединений.
-func NewISSClient() *gomoex.ISSClient {
+func NewISSClient(maxCons int) *gomoex.ISSClient {
 	client := &http.Client{
 		Transport: &http.Transport{
-			MaxConnsPerHost: 20,
+			MaxConnsPerHost: maxCons,
 		},
-		Timeout: 30 * time.Second,
 	}
 	return gomoex.NewISSClient(client)
 }
