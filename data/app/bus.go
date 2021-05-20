561ca3617495cd685a9e7812f1e8928242c6e1b2
WLM1ke <wlmike@gmail.com>
2021-05-20 12:09:45 +0300

    refactor: новая шина без связи с репозиторием

--- data/app/bus.go
+++ data/app/bus.go
@@ -3,74 +3,92 @@ package app
 import (
 	"context"
 	"fmt"
+	"github.com/WLM1ke/gomoex"
+	"go.uber.org/zap"
 	"poptimizer/data/adapters"
 	"poptimizer/data/domain"
 	"sync"
 	"time"
-
-	"go.uber.org/zap"
 )
 
-// Bus - шина для обработки команд и событий, поддерживающую интерфейс модуля приложения.
+// Bus - шина для обработки событий, поддерживающую интерфейс модуля приложения.
 //
-// Регистрирует все шаги бизнес-логики — источники команд, правила обработки доменных событий и потребителей событий.
-// Процесс работы шины поддерживает данные в актуальном состоянии.
+// Направляет входящий поток событий набору бизнес-правил, читает их исходящие события и закольцовывает их обратно на вход правил.
 type Bus struct {
-	repo             adapters.TableRepo
-	handlersTimeouts time.Duration
+	rules []domain.Rule
 
-	commands  chan domain.Command
-	events    chan domain.Event
-	consumers []chan domain.Event
-
-	wg          sync.WaitGroup
-	loopCtx     context.Context
-	loopCancel  context.CancelFunc
-	loopStopped chan struct{}
+	wg         sync.WaitGroup
+	loopCancel context.CancelFunc
 }
 
-// NewBus - создает шину для обработки команд и событий.
-func NewBus(repo adapters.TableRepo, eventBusTimeouts time.Duration) *Bus {
-	ctx, cancel := context.WithCancel(context.Background())
+// NewBus - создает шину для обработки событий.
+func NewBus(repo Repo, eventBusTimeouts time.Duration, iss *gomoex.ISSClient) *Bus {
+	rules := []domain.Rule{
+		NewUoW(repo, eventBusTimeouts),
+		domain.NewUpdateTradingDates(iss),
+	}
 
 	bus := Bus{
-		repo:             repo,
-		handlersTimeouts: eventBusTimeouts,
-
-		commands: make(chan domain.Command),
-		events:   make(chan domain.Event),
-
-		loopCtx:     ctx,
-		loopCancel:  cancel,
-		loopStopped: make(chan struct{}),
-	}
-	steps := []interface{}{
-		// Источники команд
-		domain.NewCheckTradingDay(),
-		// Правила
-
-		// Потребители сообщений
-	}
-
-	for _, step := range steps {
-		bus.register(step)
+		rules: rules,
 	}
 
 	return &bus
 }
 
-// Name - модуль приложения Bus.
-func (b *Bus) Name() string {
-	return "Bus"
-}
-
-// Start - запускает основной цикл обработки команд и событий.
+// Start - запускает работу правил и основного цикла обработки событий.
 func (b *Bus) Start(_ context.Context) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	b.loopCancel = cancel
+
+	events := make(chan domain.Event)
+	consumers := make([]chan domain.Event, 0)
+
+	for _, rule := range b.rules {
+		rule := rule
+		in := make(chan domain.Event)
+		consumers = append(consumers, in)
+
+		b.wg.Add(1)
+
+		go func() {
+			defer b.wg.Done()
+
+			zap.L().Info("Rule activated", adapters.TypeField(rule))
+			rule.Activate(ctx, in, events)
+			zap.L().Info("Rule deactivated", adapters.TypeField(rule))
+		}()
+
+	}
+
+	b.wg.Add(1)
 	go func() {
-		b.loop(b.loopCtx)
+		defer b.wg.Done()
+
+		b.loop(ctx, events, consumers)
 	}()
 
 	return nil
+}
+
+func (b *Bus) loop(ctx context.Context, events chan domain.Event, consumers []chan domain.Event) {
+	for {
+		select {
+		case event := <-events:
+
+			b.wg.Add(1)
+			go func() {
+				defer b.wg.Done()
+
+				zap.L().Info("Event", adapters.TypeField(event), zap.Stringer("id", event))
+				for _, consumer := range consumers {
+					consumer <- event
+				}
+			}()
+
+		case <-ctx.Done():
+			return
+		}
+	}
 }
 
 // Shutdown - завершает работу основного цикла обработки команд и событий.
@@ -78,95 +96,19 @@ func (b *Bus) Shutdown(ctx context.Context) error {
 	b.loopCancel()
 
 	select {
-	case <-b.loopStopped:
+	case <-b.waitCancel():
 		return nil
 	case <-ctx.Done():
 		return fmt.Errorf("bus shutdown error: %w", ctx.Err())
 	}
 }
 
-// loop осуществляет вызов обработчиков команд и событий при их наличии до отмены контекста цикла.
-func (b *Bus) loop(ctx context.Context) {
-	for {
-		b.wg.Add(1)
-		select {
-		case cmd := <-b.commands:
-			go func() {
-				defer b.wg.Done()
-				b.handleOneCommand(ctx, cmd)
-			}()
-		case event := <-b.events:
-			go func() {
-				defer b.wg.Done()
+func (b *Bus) waitCancel() chan struct{} {
+	stopped := make(chan struct{})
+	go func() {
+		b.wg.Wait()
+		close(stopped)
+	}()
 
-				go b.handleOneEvent(ctx, event)
-			}()
-		case <-ctx.Done():
-			b.wg.Done()
-			b.wg.Wait()
-			close(b.loopStopped)
-
-			return
-		}
-	}
-}
-
-// handleOneCommand - загружает таблицу, вызывает обработчик команды и направляет возникшие события в очередь.
-func (b *Bus) handleOneCommand(ctx context.Context, cmd domain.Command) {
-	zap.L().Info("Command", zap.Stringer("table", cmd.ID()))
-
-	ctx, cancel := context.WithTimeout(ctx, b.handlersTimeouts)
-
-	defer cancel()
-
-	table, err := b.repo.Load(ctx, cmd.ID())
-	if err != nil {
-		zap.L().Panic("Command", zap.Stringer("load", cmd.ID()), zap.Error(err))
-	}
-
-	for _, event := range table.HandleCommand(ctx, cmd) {
-		b.events <- event
-	}
-}
-
-// handleOneEvent - сохраняет событие, а после этого рассылает его всем потребителям событий.
-func (b *Bus) handleOneEvent(ctx context.Context, event domain.Event) {
-	zap.L().Info("Event", zap.Stringer("table", event.ID()))
-
-	ctx, cancel := context.WithTimeout(ctx, b.handlersTimeouts)
-
-	defer cancel()
-
-	if err := b.repo.Save(ctx, event); err != nil {
-		zap.L().Panic("Event", zap.Stringer("save", event.ID()), zap.Error(err))
-	}
-
-	for _, consumer := range b.consumers {
-		consumer <- event
-	}
-}
-
-// register регистрирует шаги бизнес-логики — источники команд, правила обработки доменных событий и потребителей
-// событий.
-func (b *Bus) register(step interface{}) {
-	if consumer, ok := step.(domain.EventConsumer); ok {
-		newChan := make(chan domain.Event)
-		b.consumers = append(b.consumers, newChan)
-
-		b.wg.Add(1)
-
-		go func() {
-			defer b.wg.Done()
-			consumer.StartHandleEvent(b.loopCtx, newChan)
-		}()
-	}
-
-	if source, ok := step.(domain.CommandSource); ok {
-		b.wg.Add(1)
-
-		go func() {
-			defer b.wg.Done()
-			source.StartProduceCommands(b.loopCtx, b.commands)
-		}()
-	}
+	return stopped
 }
--- data/app/bus_test.go
+++ data/app/bus_test.go
@@ -2,22 +2,25 @@ package app
 
 import (
 	"context"
+	"github.com/stretchr/testify/assert"
 	"poptimizer/data/domain"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
 )
 
-type TestEventConsumer struct {
-	flow chan<- string
+type TestRule struct {
+	flow    chan<- string
+	atStart bool
 }
 
-func (t TestEventConsumer) StartHandleEvent(ctx context.Context, source <-chan domain.Event) {
+func (t TestRule) Activate(ctx context.Context, in <-chan domain.Event, out chan<- domain.Event) {
+	if t.atStart {
+		out <- domain.NewID("g", "n")
+	}
+
 	for {
 		select {
-		case event := <-source:
-			t.flow <- "Event consumed " + event.ID().String()
+		case event := <-in:
+			t.flow <- "Event consumed " + event.String()
 		case <-ctx.Done():
 			t.flow <- "Stopped"
 
@@ -26,83 +29,18 @@ func (t TestEventConsumer) StartHandleEvent(ctx context.Context, source <-chan d
 	}
 }
 
-type TestCommandSource struct {
-	flow chan<- string
-}
-
-func (t TestCommandSource) StartProduceCommands(ctx context.Context, output chan<- domain.Command) {
-	cmd := domain.NewTableID("cg", "cn")
-	output <- cmd
-	t.flow <- "Command send"
-
-	<-ctx.Done()
-
-	t.flow <- "Stopped"
-}
-
-type TestTable struct {
-	domain.TableID
-	flow chan<- string
-}
-
-func (t TestTable) ID() domain.TableID {
-	return t.TableID
-}
-
-func (t TestTable) HandleCommand(_ context.Context, cmd domain.Command) []domain.Event {
-	t.flow <- "TableHandles " + cmd.ID().String()
-
-	event := domain.NewTableID("eg", "en")
-
-	return []domain.Event{event}
-}
-
-type TestRepo struct {
-	flow chan<- string
-}
-
-func (t TestRepo) Load(_ context.Context, id domain.TableID) (domain.Table, error) {
-	t.flow <- "TableLoad " + id.String()
-
-	return TestTable{TableID: id, flow: t.flow}, nil
-}
-
-func (t TestRepo) Save(_ context.Context, event domain.Event) error {
-	t.flow <- "Event saved " + event.ID().String()
-
-	return nil
-}
-
 func TestBusFlow(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	flow := make(chan string, 3)
+	flow := make(chan string, 2)
+	rules := []domain.Rule{TestRule{flow, false}, TestRule{flow, true}}
 
-	bus := Bus{
-		repo:             TestRepo{flow},
-		handlersTimeouts: time.Second,
-
-		commands: make(chan domain.Command),
-		events:   make(chan domain.Event),
-
-		loopCtx:     ctx,
-		loopCancel:  cancel,
-		loopStopped: make(chan struct{}),
-	}
-
-	bus.register(TestEventConsumer{flow})
-	bus.register(TestEventConsumer{flow})
-	bus.register(TestCommandSource{flow})
+	bus := Bus{rules: rules}
 
 	if bus.Start(context.Background()) != nil {
 		t.Error("Не удалось запустить шину")
 	}
 
-	assert.Equal(t, "Command send", <-flow)
-	assert.Equal(t, "TableLoad ID(cg, cn)", <-flow)
-	assert.Equal(t, "TableHandles ID(cg, cn)", <-flow)
-	assert.Equal(t, "Event saved ID(eg, en)", <-flow)
-	assert.Equal(t, "Event consumed ID(eg, en)", <-flow)
-	assert.Equal(t, "Event consumed ID(eg, en)", <-flow)
+	assert.Equal(t, "Event consumed ID(g, n)", <-flow)
+	assert.Equal(t, "Event consumed ID(g, n)", <-flow)
 
 	if bus.Shutdown(context.Background()) != nil {
 		t.Error("Не удалось остановить шину")
@@ -110,11 +48,4 @@ func TestBusFlow(t *testing.T) {
 
 	assert.Equal(t, "Stopped", <-flow)
 	assert.Equal(t, "Stopped", <-flow)
-	assert.Equal(t, "Stopped", <-flow)
-}
-
-func TestBusName(t *testing.T) {
-	bus := NewBus(TestRepo{}, time.Second)
-
-	assert.Equal(t, "Bus", bus.Name())
 }
