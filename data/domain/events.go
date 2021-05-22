629ddc74d438f7245a9a4c5d4d3c7f1268106252
WLM1ke <wlmike@gmail.com>
2021-05-22 16:51:42 +0300

    feat: событие с ошибкой

--- data/domain/events.go
+++ data/domain/events.go
@@ -2,25 +2,26 @@ package domain
 
 // UpdateRequired требуется обновление таблицы.
 //
-// Событие содержит указатель на незаполненную таблицу с правильным ID.
-// Событие должно перехватываться на уровне приложения, а данные таблицы загружаться из хранилища.
+// Событие содержит указатели на незаполненные таблицы с правильным ID, которые нужны для построения агрегата, начиная с таблицы,
+// являющейся его корнем. Событие должно перехватываться на уровне приложения, а данные таблицы загружаться из хранилища.
 type UpdateRequired struct {
-	Template Table
+	Templates []Table
 }
 
-// Group - группа таблицы.
+// Group - группа таблицы корня агрегата.
 func (u UpdateRequired) Group() Group {
-	return u.Template.Group()
+	return u.Templates[0].Group()
 }
 
-// Name - имя таблицы в рамках группы.
+// Name - имя таблицы корня агрегата в рамках группы.
 func (u UpdateRequired) Name() Name {
-	return u.Template.Name()
+	return u.Templates[0].Name()
 }
 
-// String - текстовое представление ID таблицы.
-func (u UpdateRequired) String() string {
-	return u.Template.String()
+// UpdateError - ошибка при обновлении таблицы.
+type UpdateError struct {
+	ID
+	Error error
 }
 
 // RowsReplaced - событие замены всех строк в таблице.
